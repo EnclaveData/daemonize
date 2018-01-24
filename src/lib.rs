477253521a9bd7c62a5272d57133c371fb3fe76d
Tobias Ford <toby.ford@pobox.com>
2018-01-23 21:59:34 -0500

    removed the sleep for fork debugging .. not needed for gdb

--- src/lib.rs
+++ src/lib.rs
@@ -359,7 +359,6 @@ impl<T> Daemonize<T> {
 
 unsafe fn perform_fork() -> Result<()> {
     let pid = fork();
-    thread::sleep(Duration::from_secs(60));
     if pid < 0 {
         Err(DaemonizeError::Fork)
     } else if pid == 0 {
